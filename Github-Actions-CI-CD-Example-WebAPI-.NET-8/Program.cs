3e6de2bf4311b2ea81397ed2e412e1ed8eab0947
kraftcoding <i.elg.alzaa@gmail.com>
2026-01-17 09:10:00 +0100

    Add project files.

---
+++ Github-Actions-CI-CD-Example-WebAPI-.NET-8/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+string id = Guid.NewGuid().ToString();
+string osDescription = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+DateTime startUpTime = DateTime.Now;
+const string ver = "v1.0.1";
+
+var app = builder.Build();
+
+app.MapGet("/", () => $"Id {id}\nOperating System {osDescription}\nStarted at {startUpTime}\n\nCurrent time {DateTime.Now}\nVer {ver}");
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,3 @@
+# Github Actions CI/CD Example WebAPI .NET-8
+
+In this repository, we will find an example of how to publish Docker images to a registry, such as Docker Hub with GitHub Actions, as part of your continuous integration (CI) workflow.
